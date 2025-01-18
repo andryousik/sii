3cb1a76b8e62f59c938f696640363a981bb9c502
kArteraa <mishapel20@gmail.com>
2025-01-19 01:09:24 +0300

    fix: change request logic

--- src/store/useModalStore.js
+++ src/store/useModalStore.js
@@ -3,7 +3,9 @@ import {create} from "zustand";
 const useModalStore =  create((set) => ({
     isOpen: false,
     image: null,
+    file: null,
     setImage: (image) => set({ image }), // Обновляет состояние image
+    setFile: (file) => set({ file }),
     setOpen: (open) => set(() => ({ isOpen: open})), // Добавлено для переключения isOpen
 }));
 
